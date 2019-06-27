aa819e165826f2b2b469fddc85a862021ceb60a4
vioool <violapuhalovic@gmail.com>
2019-06-27 12:21:11 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,50 @@
+var app = new Vue({
+    el: '#main',
+    data: {
+        allBooks: [],
+        allCovers: [],
+        search: '',
+
+    },
+
+    created: function () {
+        this.getData();
+        this.getCovers();
+    },
+
+    methods: {
+        getData: async function () {
+            var data =
+                await fetch("https://api.myjson.com/bins/zyv02", {
+                    method: 'GET',
+                })
+                .then(response => response.json())
+                .then(json => json)
+                .catch(err => console.error(err))
+
+            app.allBooks = data.books;
+            console.log(app.allBooks)
+            console.log(app.allCovers)
+//            this.getCovers();
+
+        },
+
+        //aleways call yourfunction otherwise its stored in a box in he corner, not exposed to any light       
+        getCovers: function () {
+            this.allCovers = [];
+            for (var i = 0; i < this.allBooks.length; i++) {
+
+                this.allCovers.push(this.allBooks[i].cover);
+            }
+        }
+    },
+    
+    computed: {
+            filteredList: function() {
+                return this.allBooks.filter(item => {
+                    return item.title.toLowerCase().includes(this.search.toLowerCase())
+                })
+            }
+        },
+
+})
